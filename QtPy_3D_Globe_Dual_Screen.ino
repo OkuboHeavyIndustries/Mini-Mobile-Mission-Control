9f56c4d7373c06685c3bd3117db8025152e3c803
OkuboHeavyIndustries <53265107+OkuboHeavyIndustries@users.noreply.github.com>
2023-06-25 09:40:08 +0000

    Update QtPy_3D_Globe_Dual_Screen.ino

--- QtPy_3D_Globe_Dual_Screen.ino
+++ QtPy_3D_Globe_Dual_Screen.ino
@@ -12,7 +12,7 @@
 
 
 
-//The following line will need changing depending on your board type!
+//The following lines will need changing depending on your screen type!
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C OLED_2(U8G2_R0, 3, 2, /* reset=*/ U8X8_PIN_NONE);
